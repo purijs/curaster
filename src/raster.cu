5df90e0730d3edf210e198aedb17420e4d05213e
purijs <jaskaranpuri@gmail.com>
2026-03-31 22:48:35 +0200

    optimize by chunks, progress bar

--- src/raster.cu
+++ src/raster.cu
@@ -7,6 +7,9 @@
 #include <cstdlib>
 #include <bits/stdc++.h>
 #include <pybind11/pybind11.h>
+#include "cpl_progress.h"
+
+const int threadsPerBlock = 256;
 
 __global__ void compute_ndvi(const float* red_band, const float* nir_band, float* ndvi_band, int size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -15,7 +18,10 @@ __global__ void compute_ndvi(const float* red_band, const float* nir_band, float
     }
 }
 
-void curaster(std::string& input_file, std::string& output_file, int& red_band_index, int& nir_band_index) {
+void curaster(std::string& input_file, std::string& output_file, int& red_band_index, int& nir_band_index, int& chunk_size, bool& verbose) {
+
+    const int TILE_HEIGHT = chunk_size;
+
     GDALAllRegister();
     GDALDataset* rasterfile = (GDALDataset*)GDALOpen(input_file.c_str(), GA_ReadOnly);
 
@@ -23,57 +29,15 @@ void curaster(std::string& input_file, std::string& output_file, int& red_band_i
     GDALRasterBand* band_nir = rasterfile->GetRasterBand(nir_band_index);
 
     int blockXSize = band_red->GetXSize(), blockYSize = band_red->GetYSize();
-    std::vector<float>* buffer_red_pixels = new std::vector<float>(blockXSize * blockYSize);
-    std::vector<float>* buffer_nir_pixels = new std::vector<float>(blockXSize * blockYSize);
-
-    CPLErr err = band_red->RasterIO(
-        GF_Read,
-        0, 0,
-        blockXSize, blockYSize,
-        buffer_red_pixels->data(),
-        blockXSize, blockYSize,
-        GDT_Float32,
-        0, 0
-    );
-
-    CPLErr err_nir = band_nir->RasterIO(
-        GF_Read,
-        0, 0,
-        blockXSize, blockYSize,
-        buffer_nir_pixels->data(),
-        blockXSize, blockYSize,
-        GDT_Float32,
-        0, 0
-    );
+    std::vector<float>* buffer_red_pixels = new std::vector<float>(TILE_HEIGHT * blockXSize);
+    std::vector<float>* buffer_nir_pixels = new std::vector<float>(TILE_HEIGHT * blockXSize);
+    std::vector<float>* buffer_ndvi_pixels = new std::vector<float>(TILE_HEIGHT * blockXSize);
 
     float* gpu_buffer_red_pixels, *gpu_buffer_nir_pixels, *gpu_buffer_ndvi_pixels;
-    
-    cudaMalloc((void**)&gpu_buffer_red_pixels, buffer_red_pixels->size() * sizeof(float));
-    cudaMemcpy(gpu_buffer_red_pixels, buffer_red_pixels->data(), buffer_red_pixels->size() * sizeof(float), cudaMemcpyHostToDevice);
 
-    cudaMalloc((void**)&gpu_buffer_nir_pixels, buffer_nir_pixels->size() * sizeof(float));
-    cudaMemcpy(gpu_buffer_nir_pixels, buffer_nir_pixels->data(), buffer_nir_pixels->size() * sizeof(float), cudaMemcpyHostToDevice);
-
-    cudaMalloc((void**)&gpu_buffer_ndvi_pixels, buffer_red_pixels->size() * sizeof(float));
-
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (buffer_red_pixels->size() + threadsPerBlock - 1) / threadsPerBlock;
-
-    compute_ndvi<<<blocksPerGrid, threadsPerBlock>>>(gpu_buffer_red_pixels, gpu_buffer_nir_pixels, gpu_buffer_ndvi_pixels, buffer_red_pixels->size());
-    cudaDeviceSynchronize();
-
-    std::vector<float>* buffer_ndvi_pixels = new std::vector<float>(blockXSize * blockYSize);
-    cudaMemcpy(buffer_ndvi_pixels->data(), gpu_buffer_ndvi_pixels, buffer_ndvi_pixels->size() * sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaFree(gpu_buffer_red_pixels);
-    cudaFree(gpu_buffer_nir_pixels);
-
-    delete buffer_red_pixels;
-    delete buffer_nir_pixels;
-
-    cudaFree(gpu_buffer_ndvi_pixels);
-
-    std::cout << "Writing NDVI to disk..." << std::endl;
+    cudaMalloc((void**)&gpu_buffer_red_pixels, TILE_HEIGHT * blockXSize * sizeof(float));
+    cudaMalloc((void**)&gpu_buffer_nir_pixels, TILE_HEIGHT * blockXSize * sizeof(float));
+    cudaMalloc((void**)&gpu_buffer_ndvi_pixels, TILE_HEIGHT * blockXSize * sizeof(float));
 
     double geoTransform[6];
     rasterfile->GetGeoTransform(geoTransform);
@@ -81,32 +45,74 @@ void curaster(std::string& input_file, std::string& output_file, int& red_band_i
 
     GDALDriver* gtiffDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
     GDALDataset* outRaster = gtiffDriver->Create(
-        output_file.c_str(),
-        blockXSize,
-        blockYSize,
-        1,
-        GDT_Float32,
-        nullptr
+        output_file.c_str(), blockXSize, blockYSize, 1, GDT_Float32, nullptr
     );
+    outRaster->SetGeoTransform(geoTransform);
+    outRaster->SetProjection(projection);
+    GDALRasterBand* outBand = outRaster->GetRasterBand(1);
 
-    if (outRaster != nullptr) {
-        outRaster->SetGeoTransform(geoTransform);
-        outRaster->SetProjection(projection);
+    std::cout << "Writing NDVI to disk..." << std::endl;
 
-        CPLErr writeErr = outRaster->GetRasterBand(1)->RasterIO(
-            GF_Write,
-            0, 0,
-            blockXSize, blockYSize,
-            buffer_ndvi_pixels->data(),
-            blockXSize, blockYSize,
+    double progress = -1.0;
+    if (verbose) {
+        GDALTermProgress(0.0, nullptr, &progress);
+    }
+
+    for (int y_offset = 0; y_offset < blockYSize; y_offset += TILE_HEIGHT) {
+        int current_tile_height = std::min(TILE_HEIGHT, blockYSize - y_offset);
+        size_t current_pixels = current_tile_height * blockXSize;
+
+        band_red->RasterIO(
+            GF_Read,
+            0, y_offset,
+            blockXSize, current_tile_height,
+            buffer_red_pixels->data(),
+            blockXSize, current_tile_height,
             GDT_Float32,
             0, 0
         );
 
-        GDALClose(outRaster);
-    }
+        band_nir->RasterIO(
+            GF_Read,
+            0, y_offset,
+            blockXSize, current_tile_height,
+            buffer_nir_pixels->data(),
+            blockXSize, current_tile_height,
+            GDT_Float32,
+            0, 0
+        );
     
+        cudaMemcpy(gpu_buffer_red_pixels, buffer_red_pixels->data(), current_pixels * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(gpu_buffer_nir_pixels, buffer_nir_pixels->data(), current_pixels * sizeof(float), cudaMemcpyHostToDevice);
+
+        const int blocksPerGrid = (current_pixels + threadsPerBlock - 1) / threadsPerBlock;
+        
+        compute_ndvi<<<blocksPerGrid, threadsPerBlock>>>(gpu_buffer_red_pixels, gpu_buffer_nir_pixels, gpu_buffer_ndvi_pixels, current_pixels);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(buffer_ndvi_pixels->data(), gpu_buffer_ndvi_pixels, current_pixels * sizeof(float), cudaMemcpyDeviceToHost);
+
+        outRaster->GetRasterBand(1)->RasterIO(
+            GF_Write, 0, y_offset, blockXSize, current_tile_height,
+            buffer_ndvi_pixels->data(), blockXSize, current_tile_height, GDT_Float32, 0, 0
+        );
+
+        if (verbose) {
+            double fraction_complete = (double)(y_offset + current_tile_height) / blockYSize;
+            GDALTermProgress(fraction_complete, nullptr, &progress);
+        }
+
+    }
+
+    cudaFree(gpu_buffer_red_pixels);
+    cudaFree(gpu_buffer_nir_pixels);
+    cudaFree(gpu_buffer_ndvi_pixels);
+
+    delete buffer_red_pixels;
+    delete buffer_nir_pixels;
     delete buffer_ndvi_pixels;
+
+    GDALClose(outRaster);
     GDALClose(rasterfile);
 }
 
@@ -115,6 +121,8 @@ PYBIND11_MODULE(curaster, m) {
         pybind11::arg("input_file"),
         pybind11::arg("output_file"),
         pybind11::arg("red_band_index"),
-        pybind11::arg("nir_band_index")
+        pybind11::arg("nir_band_index"),
+        pybind11::arg("chunk_size") = 256,
+        pybind11::arg("verbose") = true
     );
 }
